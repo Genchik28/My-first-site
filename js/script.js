62215ece6a1cf05311db5a6869e0f7b06bf1d2aa
Genchik Kostenko <genkakostenko0409@gmail.com>
2023-11-17 10:26:47 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $('.container-slide').slick({
+    arrow:false,
+    dots:true,
+    adaptiveHeight:true,
+    slidesToShow:3,
+    slidesToScroll:3,
+    pauseOnDotsHover:true,
+    draggable:true,
+    waitForAnimate:false,
+    
+  }); 
+});
+
+
+  
+
+
